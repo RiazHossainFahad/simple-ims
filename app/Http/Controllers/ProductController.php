cad57fcea017da2cef482589affe785012d725c4
RiazHossainFahad <mdfahad6562@gmail.com>
2020-01-18 12:19:02 +0600

    dashboard added for user and supplier

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\User;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance. check if the user is
+     * authenticated or not
      *
-     * @return \Illuminate\Http\Response
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        //
+        //Get User by id for getting userType
+        $user = User::where('id', auth()->id())->first();
+        //Get the type
+        $userType = $user->is_supplier;
+
+        switch ($userType) {
+            //execute, if the user is not a supplier
+            case 0:
+                $products = Product::where('need_supply', 0)->get();
+                break;
+
+            //execute, if the user is not supplier
+            case 1:
+                $products = Product::where('need_supply', 1)->get();
+                break;
+        }
+
+        // return $products;
+        // return view with all the products
+        return view('home')->withProducts($products);
     }
 
     /**
